2ac1e951dee812713d538951b954330816d5c055
Dexy <31504744+DexyThePuppy@users.noreply.github.com>
2025-08-27 13:05:21 +0200

    use valid semantic version

--- CustomPlatformColors/CustomPlatformColors.cs
+++ CustomPlatformColors/CustomPlatformColors.cs
@@ -22,7 +22,7 @@ namespace CustomPlatformColors
         public override string Name => "CustomPlatformColors";
         public override string Author => "Dexy";
         public override string Version => VERSION_CONSTANT;
-        public const string VERSION_CONSTANT = "1.2";
+        public const string VERSION_CONSTANT = "1.2.0";
         public override string Link => "https://github.com/DexyThePuppy/CustomPlatformColors";
 
         //The following
